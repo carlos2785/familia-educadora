1034ce16f0144a9a7e4d8db05cea75cff31ad2d9
Carlos <carlosmolina2785@gmail.com>
2024-01-01 19:30:10 -0500

    frontend-mostrar-notas

--- familia-frontend/src/components/MostrarRegistro.js
+++ familia-frontend/src/components/MostrarRegistro.js
@@ -1,37 +1,78 @@
 import React,{useState,useEffect} from 'react';
-import { SeleccionarFechaGrado } from './SeleccionarFechaGrado';
+import { BuscarNotas } from './BuscarNotas';
+import { obtenerFecha, obtenerGrado } from '../funciones/funciones';
+
 
 export const MostrarRegistro=()=>{
+
+    const [selectFecha, setSelectFecha] = useState();//variable de estado para guardar la fecha seleccionada
+    const [selectGrado, setSelectGrado] = useState();//variable de estado para guardar el grado seleccionado
+    const [fechas, setFechas] = useState([]);//variable de estado para guardar las fechas
+    const [grados, setGrados] = useState([]);//variable de estado para guardar los grados
+    
+
+    useEffect(()=>{
+        obtenerFecha(setFechas);
+        //obtenerGrado(setGrados);
+    },[])
+    useEffect(()=>{
+        //obtenerFecha(setFechas);
+        obtenerGrado(setGrados);
+    },[])
+
+    //función que va dentro del onClick
+    const handleSetSelectFecha=(e)=>{
+        const SelectFecha=e.target.value;//si selecciona una lista dentro del select, toma ese valor y lo guarda en la variable de estado
+        setSelectFecha(SelectFecha);
+        //console.log(selectFecha);
+    }
+    const handleSetSelectGrado=(e)=>{
+        const SelectGrado=e.target.value;//si selecciona una lista dentro del select, toma ese valor y lo guarda en la variable de estado
+        setSelectGrado(SelectGrado);
+    }
+
     return(
         <div className='container'>
-            <div>
-                <SeleccionarFechaGrado/>
-            </div>
-            <div className="mt-3 mb-3 row row-cols-1 row-cols-md-1 g-4">
-                <div>
-                    <table className="table">
-                        <thead>
-                            <tr>
-                            <th scope="col">Doc</th>
-                            <th scope="col">Apellidos</th>
-                            <th scope="col">Nombres</th>
-                            <th scope="col">Grado</th>
-                            <th scope="col">Nota</th>
-                            <th scope="col">Fecha</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>601</td>
-                            <td>5</td>
-                            <td>2023-10-8</td>
-                            </tr>
-                        </tbody>
-                    </table>
+            <div className='container'>
+                <div className="mt-2 mb-2 row row-cols-1 row-cols-md-2 g-4">
+                    <div>
+                        <label>
+                            Fecha del taller:
+                            <select className="form-select" aria-label="Default select example"
+                            value={selectFecha} //aquí se guarda la fecha seleccionada
+                            onChange={handleSetSelectFecha}//el evento onChange permite manejar el evento de 
+                            //seleccionar la fecha
+                            >
+                                <option >Fecha</option>
+                                {fechas.map((fecha, index) => (
+                                    <option key={index} value={fecha.fecha}>{fecha.fecha}</option>
+                                ))}
+                            </select>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Selecciona el grado:
+                            <select className="form-select" aria-label="Default select example"
+                            value={selectGrado} //aquí se guarda la fecha seleccionada
+                            onChange={handleSetSelectGrado}
+                            >
+                                <option >Grado</option>
+                                {grados.map((grado, index)=>(
+                                    <option key={index} value={grado.grado}>{grado.grado}</option>
+                                ))}
+                            </select>
+                        </label>
+                    </div>
                 </div>
+            </div>
+            <div>
+            <BuscarNotas
+                selectFecha={selectFecha}
+                setSelectFecha={setSelectFecha}
+                selectGrado={selectGrado}
+                setSelectGrado={setSelectGrado}
+            />
             </div>
         </div> 
     );
