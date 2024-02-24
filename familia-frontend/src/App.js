e495bb3327c9e04f2160fd42b55410448886b47d
Carlos <carlosmolina2785@gmail.com>
2024-02-23 22:53:43 -0500

    Rutas protegidas 2.0

--- familia-frontend/src/App.js
+++ familia-frontend/src/App.js
@@ -8,63 +8,46 @@ import { Registro } from './components/Registro/Registro';
 import { Login } from './components/Login/Login';
 import PrivateRoute from './routes/PrivateRoute';
 
+const NotFound = () => {
+  return <div>Página no encontrada</div>;
+};
+
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    // Verifica si el usuario tiene un token almacenado
     const token = localStorage.getItem('token');
     setIsAuthenticated(!!token);
   }, []);
 
   const handleLogin = (token) => {
-    // Almacena el token en el localStorage y actualiza el estado de autenticación
     localStorage.setItem('token', token);
     setIsAuthenticated(true);
   };
 
   const handleLogout = () => {
-    // Limpiar el token almacenado y actualizar el estado de autenticación
     localStorage.removeItem('token');
     setIsAuthenticated(false);
-    // Redirigir a la página de inicio de sesión (Login)
-    window.location.href = '/login';
     console.log('Cerrando sesión...');
   };
 
   return (
     <Router>
+      {isAuthenticated && <Header onLogout={handleLogout} />}
       <Switch>
-        <Route path='/login'>
-          <Login onLogin={handleLogin} />
+        <Route exact path='/login'>
+          {isAuthenticated ? <Redirect to='/registro' /> : <Login onLogin={handleLogin} />}
         </Route>
-        <Route path='/'>
-          <Header onLogout={handleLogout} />
-          <Switch>
-            <PrivateRoute
-              path='/notas'
-              component={NotasPorGrados}
-              isAuthenticated={isAuthenticated}
-            />
-            <PrivateRoute
-              path='/estudiantes'
-              component={ListarEstudiantes}
-              isAuthenticated={isAuthenticated}
-            />
-            <PrivateRoute
-              path='/padres'
-              component={ListarPadres}
-              isAuthenticated={isAuthenticated}
-            />
-            <PrivateRoute
-              path='/registro'
-              component={Registro}
-              isAuthenticated={isAuthenticated}
-            />
-            <Route exact path='/' component={Registro} />
-            <Redirect to='/' />
-          </Switch>
-        </Route>
+        {isAuthenticated && (
+          <>
+            <PrivateRoute exact path='/notas' component={NotasPorGrados} isAuthenticated={isAuthenticated} />
+            <PrivateRoute exact path='/estudiantes' component={ListarEstudiantes} isAuthenticated={isAuthenticated} />
+            <PrivateRoute exact path='/padres' component={ListarPadres} isAuthenticated={isAuthenticated} />
+            <PrivateRoute exact path='/registro' component={Registro} isAuthenticated={isAuthenticated} />
+          </>
+        )}
+        <Route exact path='/404' component={NotFound} />
+        <Redirect to={isAuthenticated ? '/404' : '/login'} />
       </Switch>
     </Router>
   );
