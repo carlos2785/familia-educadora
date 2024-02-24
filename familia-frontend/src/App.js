ec53b14c8d33a2278901d62646dace4fd0a24d07
Carlos <carlosmolina2785@gmail.com>
2024-02-23 21:35:35 -0500

    Rutas protegidas v1

--- familia-frontend/src/App.js
+++ familia-frontend/src/App.js
@@ -1,44 +1,73 @@
-import React from "react";
-import { Header } from './components/Header/Header';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { Header } from './components/Header/Header';
 import { NotasPorGrados } from "./components/Notas/NotasPorGrados";
 import { ListarEstudiantes } from "./components/Estudiantes/ListarEstudiantes";
 import { ListarPadres } from './components/Padres/ListarPadres';
 import { Registro } from './components/Registro/Registro';
 import { Login } from './components/Login/Login';
+import PrivateRoute from './routes/PrivateRoute';
 
 const App = () => {
-    const handleLogout = () => {
-        // Limpiar el token almacenado
-        localStorage.removeItem('token');
-        // Redirigir a la página de inicio de sesión (Login)
-        window.location.href = '/login';  // Usa window.location.href para una redirección inmediata y evitar problemas de enrutamiento
-        console.log('Cerrando sesión...');
-    };
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    return (
-        <Router>
-            <Switch>
-                <Route path='/login'>
-                    {/* Pasa la función handleLogout al componente Login */}
-                    <Login onLogin={(token) => console.log('Token recibido:', token)} />
-                </Route>
-                <Route path='/'>
-                    {/*<Header onLogout={(history) => handleLogout(history)} />*/}
-                    {/*<Header onLogout={() => handleLogout()} />*/}
-                    <Header onLogout={handleLogout} />
-                    <Switch>
-                        <Route exact path='/' component={Registro} />
-                        <Route exact path='/notas' component={NotasPorGrados} />
-                        <Route exact path='/estudiantes' component={ListarEstudiantes} />
-                        <Route exact path='/padres' component={ListarPadres} />
-                        <Route exact path='/registro' component={Registro} />
-                        <Redirect to='/' />
-                    </Switch>
-                </Route>
-            </Switch>
-        </Router>
-    );
+  useEffect(() => {
+    // Verifica si el usuario tiene un token almacenado
+    const token = localStorage.getItem('token');
+    setIsAuthenticated(!!token);
+  }, []);
+
+  const handleLogin = (token) => {
+    // Almacena el token en el localStorage y actualiza el estado de autenticación
+    localStorage.setItem('token', token);
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    // Limpiar el token almacenado y actualizar el estado de autenticación
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
+    // Redirigir a la página de inicio de sesión (Login)
+    window.location.href = '/login';
+    console.log('Cerrando sesión...');
+  };
+
+  return (
+    <Router>
+      <Switch>
+        <Route path='/login'>
+          <Login onLogin={handleLogin} />
+        </Route>
+        <Route path='/'>
+          <Header onLogout={handleLogout} />
+          <Switch>
+            <PrivateRoute
+              path='/notas'
+              component={NotasPorGrados}
+              isAuthenticated={isAuthenticated}
+            />
+            <PrivateRoute
+              path='/estudiantes'
+              component={ListarEstudiantes}
+              isAuthenticated={isAuthenticated}
+            />
+            <PrivateRoute
+              path='/padres'
+              component={ListarPadres}
+              isAuthenticated={isAuthenticated}
+            />
+            <PrivateRoute
+              path='/registro'
+              component={Registro}
+              isAuthenticated={isAuthenticated}
+            />
+            <Route exact path='/' component={Registro} />
+            <Redirect to='/' />
+          </Switch>
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export { App };
